1257149d205fcd13c6e7aff94694c9400cfc5a5c
Nikita Orliak <nikitonorl@gmail.com>
2023-10-23 14:43:24 +0200

    Update modules

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { Root } from './Root';
+import 'bulma/css/bulma.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './App.scss';
+
+const container = document.getElementById('root') as HTMLElement;
+
+createRoot(container).render(
+  <Root />,
+);
